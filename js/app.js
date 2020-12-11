a54981d35251ea7e83645500ba9c0f4ef86676fd
Alan Hogarth <alan.hogarth@outlook.com>
2020-12-11 16:04:26 +0000

    MVP

---
+++ js/app.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newForm = document.querySelector('#new-item-form');
+    newForm.addEventListener('submit', handleNewFormSubmit);
+
+    const deleteButtonItem = document.createElement('button');
+    deleteButtonItem.id = 'delete';
+    deleteButtonItem.textContent = "delete all"
+    document.body.appendChild(deleteButtonItem);
+
+    const button = document.querySelector('#delete');
+    button.addEventListener('click', handleDelete);
+
+})
+
+const handleNewFormSubmit = function (event) {
+    event.preventDefault();
+
+    const poetryListItem = createPoetryListItem(event.target);
+    const poetryList = document.querySelector('#poetry-list');
+    poetryList.appendChild(poetryListItem);
+
+    event.target.reset();
+}
+
+const createPoetryListItem = function (form) {
+    const poetryListItem = document.createElement('li');
+    poetryListItem.classList.add('poetry-list-item')
+
+    const title = document.createElement('h2');
+    title.textContent = `Title ${form.title.value}`;
+    poetryListItem.appendChild(title);
+
+    const author = document.createElement('h3');
+    author.textContent = `Author: ${form.author.value}`;
+    poetryListItem.appendChild(author);
+
+    const subgenre = document.createElement('h4');
+    subgenre.textContent = `Subgenre: ${form.subgenre.value}`;
+    poetryListItem.appendChild(subgenre);
+
+    return poetryListItem;
+
+}
+
+const handleDelete = function (event) {
+    const poetryList = document.querySelector('#poetry-list');
+    poetryList.innerHTML = '';
+
+}
